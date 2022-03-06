1c59c30ad95447b651b216bd1965f437ad956e00
Daniel Wyrzykowski <kovsky0@users.noreply.github.com>
2022-03-06 11:16:42 +0100

    deploy-5_3_2022_5_48_PM (#76)

---
+++ pages/ukraine/thank-you.tsx
@@ -0,0 +1,104 @@
+import { StandardLayout } from "../../components/layouts"
+import React, { useEffect, useState } from "react"
+import "../../lib/canvas-confetti/confetti"
+import ShareModal from "../../components/partials/ShareModal"
+import { api2 } from "utils/api-url"
+import Link from "next/link"
+
+const ThankYou = () => {
+  const [isShareModalOpen, setIsShareModalOpen] = useState(false)
+  const [token, setToken] = useState("")
+  const getUrlToShare = () => {
+    if (typeof window === "undefined") {
+      return ""
+    }
+    return window.origin + "/ukraine"
+  }
+  useEffect(() => {
+    const confetti = (window as any).confetti
+
+    var count = 200
+    var defaults = {
+      origin: { y: 0.7 }
+    }
+
+    function fire(particleRatio, opts) {
+      confetti(
+        Object.assign({}, defaults, opts, {
+          particleCount: Math.floor(count * particleRatio)
+        })
+      )
+    }
+
+    setTimeout(() => {
+      fire(0.25, {
+        spread: 26,
+        startVelocity: 55
+      })
+      fire(0.2, {
+        spread: 60
+      })
+      fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8
+      })
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2
+      })
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 45
+      })
+    }, 100)
+  }, [])
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return
+    }
+
+    const urlParams = new URLSearchParams(window.location.search)
+    const checkoutSessionId = urlParams.get("session_id")
+
+    api2.get(`/direct-donation/token?checkoutSessionId=${checkoutSessionId}`).then((res) => {
+      setToken(res.data)
+    })
+  }, [])
+
+  return (
+    <StandardLayout withMenu={true} withoutMenuBorder={true}>
+      <div className="thank-you">
+        <div className="thank-you__flag-top">
+          <p>#StandWithUkraine</p>
+          <h2>Thank you for your support!</h2>
+        </div>
+        <div className="thank-you__flag-bottom">
+          <button
+            className="button button--gray ukraine__share-button"
+            onClick={() => setIsShareModalOpen(true)}
+          >
+            <img src="/images/share.svg" alt="Share icon" />
+            Share
+          </button>
+
+          <Link href={`/ukraine/claim?token=${token}`}>
+            <button className="button button--gray ukraine__share-button" style={{ marginTop: 10 }}>
+              Claim your free apps
+            </button>
+          </Link>
+        </div>
+      </div>
+      <ShareModal
+        isOpen={isShareModalOpen}
+        onClose={() => setIsShareModalOpen(false)}
+        url={getUrlToShare()}
+      />
+    </StandardLayout>
+  )
+}
+
+export default ThankYou
